9bfd0514c04b8d7fcc1171ae252825b230faf1bf
Andrei Chisilenco <Andrei.Chisilenco@coherent.com>
2024-07-15 18:38:17 +0300

    Change access for properties.

--- Rover/badCode.cs
+++ Rover/badCode.cs
@@ -6,9 +6,9 @@ namespace MarsMission
 
     public class Rover
     {
-        public int X { get; set; }
-        public int Y { get; set; }
-        public char Direction { get; set; }
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public char Direction { get; private set; }
 
         public Rover(int x, int y, char direction)
         {
